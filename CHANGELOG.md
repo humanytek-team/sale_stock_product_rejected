f41d0ebca59b00b772d870c3b86f13e808c842ad
Manuel Marquez <buzondemam@gmail.com>
2017-10-14 19:39:25 -0400

    [ADD] Adds method _onchange_product_id_check_stock of model sale.order.line to register automatically a negation for a product that is intended to be sold but has no stock.

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+### added
+- Adds method _onchange_product_id_check_stock of model sale.order.line to register automatically a negation for a product that is intended to be sold but has no stock.
---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Record automatically a negation for a product on a sale',
+    'description': '''Records automatically a negation (product_negation) for a
+    product that is intended to be sold but has no stock.''',
+    'version': '9.0.0.1.0',
+    'category': 'Sales',
+    'author': 'Humanytek',
+    'website': "http://www.humanytek.com",
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_stock',
+        'sale_stock_dates_next_receptions',
+        'product_rejected'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False
+}
